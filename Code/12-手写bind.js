a2b4026676cf4885d0aa176df1308199cd349876
dingxuewu <2497436726@qq.com>
2021-07-14 22:39:54 +0800

    手写bind函数

---
+++ Code/12-手写bind.js
@@ -0,0 +1,21 @@
+function fn1(a, b, c) {
+  console.log("this", this)
+  console.log(a, b, c)
+  return "this is fn1"
+}
+// const fn2 = fn1.bind({ x: 100 }, 10, 20, 30)
+// fn2()
+
+Function.prototype.bind1 = function () {
+  // 获取参数数组
+  const args = Array.prototype.slice.call(arguments)
+  // 获取第一项
+  const t = args.shift()
+  //fn1.bind(...)中的bind
+  const self = this
+  return function () {
+    return self.apply(t, args)
+  }
+}
+const fn3 = fn1.bind1({ x: 100 }, 10, 20, 30)
+fn3()
