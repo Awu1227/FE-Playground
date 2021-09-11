01524aba1fbf74f19e4e5d96a6f1aefd842bba15
dingxuewu <2497436726@qq.com>
2021-09-11 20:56:41 +0800

    跨域

--- JavaScript/跨域.js
+++ JavaScript/跨域.js
@@ -1,3 +1,9 @@
+// 跨域是由于浏览器的同源策略引起的，协议，域名和端口号有一个不同就是跨域，主要是为了防止CSRF攻击
+
+// 解决方案：
+// JSONP , CORS
+
+// JSONP
 function jsonp(url, jsonpCallback, success) {
   let script = document.createElement('script')
   script.src = url
@@ -12,3 +18,7 @@ function jsonp(url, jsonpCallback, success) {
 jsonp('http://xxx', 'callback', function (value) {
   console.log(value);
 })
+
+// CORS
+// 需要浏览器和后端同时支持
+// 服务器端设置Access-Control-Allow-Origin就可以开启CORS
