530dd4a1e586f10f92aeea11c496e703db27a35e
dingxuewu <2497436726@qq.com>
2021-09-18 20:13:49 +0800

    手写深拷贝

--- JavaScript/07-深拷贝.js
+++ JavaScript/07-深拷贝.js
@@ -11,11 +11,9 @@ obj2 = deepClone(obj1)
 obj2.name = "柚子"
 console.log(obj1.name)
 function deepClone(obj) {
-  // obj是null或者不是对象和数组直接返回
-  if (typeof obj !== 'object'|| obj == null) {
+  if (typeof obj === 'object' || typeof obj == null) {
     return obj
   }
-  // 初始化返回结果
   let result
   if (obj instanceof Array) {
     result = []
@@ -23,9 +21,8 @@ function deepClone(obj) {
     result = {}
   }
   for (let key in obj) {
-    // 保证key不是原型上的属性
     if (obj.hasOwnProperty(key)) {
-      result[key] = deepClone(obj[key])
+      result[key] = deepClone(obj.key)
     }
   }
   return result
