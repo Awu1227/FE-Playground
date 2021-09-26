97e8c7d56191a06cbf9baef95abd8716fd8243d7
dingxuewu <2497436726@qq.com>
2021-09-26 16:44:58 +0800

    手写call函数

--- JavaScript/手写call函数.js
+++ JavaScript/手写call函数.js
@@ -2,8 +2,7 @@ Function.prototype.call = function (context, ...args) {
   let context = context || window
   let fn = Symbol('fn')
   context.fn = this
-  let result = eval('context.fn(...args)');
-
+  let result = eval('contex.fn(...args)')
   delete context.fn
   return result
 }
