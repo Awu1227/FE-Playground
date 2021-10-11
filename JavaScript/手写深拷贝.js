4ab2a73982ada10ad5828aad7779abb9b621ea97
dingxuewu <2497436726@qq.com>
2021-10-11 15:37:22 +0800

    fix: bug fixed

--- JavaScript/手写深拷贝.js
+++ JavaScript/手写深拷贝.js
@@ -11,7 +11,7 @@ obj2 = deepClone(obj1)
 obj2.name = "柚子"
 console.log(obj1.name)
 function deepClone(obj) {
-  if (typeof obj === 'object' || obj == null) {
+  if (typeof obj !== 'object' || obj == null) {
     // obj是null，或者不是对象和数组，直接返回
     return obj
   }
@@ -26,7 +26,7 @@ function deepClone(obj) {
     // 保证key不是原型的属性
     if (obj.hasOwnProperty(key)) {
       // 递归调用
-      result[key] = deepClone(obj.key)
+      result[key] = deepClone(obj[key])
     }
   }
   return result
