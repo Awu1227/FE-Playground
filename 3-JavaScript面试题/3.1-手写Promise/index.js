d6435bca69cee8e85d6a9edaa78d471b3582dfc7
orange <2497436726@qq.com>
2023-01-26 10:05:12 +0800

    :art:手写Promise之then调用

--- 3-JavaScript面试题/3.1-手写Promise/index.js
+++ 3-JavaScript面试题/3.1-手写Promise/index.js
@@ -31,12 +31,71 @@ class MyPromise {
   }
 
   then(fn1, fn2) {
+    fn1 = typeof fn1 === "function" ? fn1 : (v) => v;
+    fn2 = typeof fn2 === "function" ? fn2 : (e) => e;
+
     // 当pending状态下，fn1 fn2会被存储到callbacks中
+    if (this.state === "pending") {
+      return new MyPromise((resolve, reject) => {
+        this.resolveCallbacks.push(() => {
+          try {
+            const newValue = fn1(this.value);
+            resolve(newValue);
+          } catch (error) {
+            reject(error);
+          }
+        });
+
+        this.rejectCallbacks.push(() => {
+          try {
+            const newReason = fn2(this.reason);
+            reject(newReason);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      });
+    }
+
+    if (this.state === "resolved") {
+      return new MyPromise((resolve, reject) => {
+        try {
+          const newValue = fn1(this.value);
+          resolve(newValue);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }
+
+    if (this.state === "rejected") {
+      return new MyPromise((resove, reject) => {
+        try {
+          const newReason = fn2(this.reason);
+          reject(newReason);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }
+  }
+
+  // then的语法糖
+  catch(fn) {
+    return this.then(null, fn);
   }
 }
 
 const p1 = new MyPromise((resolve, reject) => {
-  // resolve(100);
+  resolve(100);
   // reject(300);
 });
+
 console.log(p1);
+
+p1.then((res) => {
+  console.log(res);
+  return res + 1;
+}).then((res) => {
+  console.log(res);
+});
