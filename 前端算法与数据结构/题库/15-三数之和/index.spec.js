833b3595b4e4876b3731b718212593d1ab215362
orange <2497436726@qq.com>
2023-02-20 11:29:48 +0800

    :speech_balloon:添加题目地址

--- 前端算法与数据结构/题库/15-三数之和/index.spec.js
+++ 前端算法与数据结构/题库/15-三数之和/index.spec.js
@@ -2,6 +2,7 @@ import threeSum from "./index";
 
 /**
  * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
+ * https://leetcode.cn/problems/3sum/description/
  */
 describe("三数之和", () => {
   it("示例1, nums = [-1,0,1,2,-1,-4]", () => {
